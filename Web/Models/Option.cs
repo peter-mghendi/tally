a676c570041a4249c85952b803b036a7ce571002
sixpeteunder <plenjo15@gmail.com>
2022-05-10 14:50:38 +0300

    Move result calculations to client side

--- Web/Models/Option.cs
+++ Web/Models/Option.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace Web.Models;
 
 public class Option
@@ -5,6 +7,10 @@ public class Option
     public int Id { get; set; }
     public string Text { get; set; } = null!;
     public Poll Poll { get; set; } = null!;
+    
+    [JsonIgnore]
     public IList<CachedVote> CachedVotes { get; set; } = new List<CachedVote>();
+    
+    [JsonIgnore]
     public IList<LiveVote> LiveVotes { get; set; } = new List<LiveVote>();
 }
