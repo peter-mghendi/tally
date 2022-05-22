f1549ca7a0985f9e49374474069abe6335d9fc5f
sixpeteunder <plenjo15@gmail.com>
2022-05-22 22:33:07 +0300

    Initial home-page feed implementation

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -1,18 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models;
 
 namespace Web.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly TallyContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty] 
+    public List<Poll> Polls { get; set; }
+    
+    public IndexModel(ILogger<IndexModel> logger, TallyContext context)
     {
         _logger = logger;
+        _context = context;
+
+        Polls = new List<Poll>();
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGet()
     {
+        var query = from poll in _context.Polls.Include(p => p.ChannelPolls)
+            .Include(p => p.Creator) 
+            orderby poll.StartedAt descending 
+            select poll;
+        Polls = await query.ToListAsync();
+
+        return Page();
     }
 }
